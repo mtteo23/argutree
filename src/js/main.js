7ead764333c765e6fc2523e69fb52cb66ffe1d18
Makte0 <matteo.pascali06@gmail.com>
2025-04-30 19:56:03 +0200

    delete and modify projects 7, test

--- src/js/main.js
+++ src/js/main.js
@@ -127,9 +127,8 @@ async function getTreeId(name) {
     const { data, error } = await supabaseClient
       .from('Tree')
       .select('id');
-      
-      //.eq('name', name)
-      //.eq('user', user)
+      .eq('user', user);
+      //.eq('name', name);
       //.single(); // Assuming "name" and "user" uniquely identify a row
     
      console.log('Tree: ', data);
