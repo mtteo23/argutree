d7a09697ae0e6a823b0cef133fa90e0f7c26e2e6
Makte0 <matteo.pascali06@gmail.com>
2025-05-01 16:24:58 +0200

    banner 2

--- src/js/banner.js
+++ src/js/banner.js
@@ -1,12 +1,10 @@
 function loadBanner() {
     const banner = `
-        <div id="banner">
-            <a href="index.html" align="center">Main Page</a>
-            <a href="explore.html">Explore</a>
-            <a href="login.html">Log in</a>
-            <a id="signup" href="signup.html">Sign up</a>
-        </div>
+      <a href="index.html" align="center">Main Page</a>
+      <a href="explore.html">Explore</a>
+      <a href="login.html">Log in</a>
+      <a id="signup" href="signup.html">Sign up</a>
     `;
     document.getElementById("banner").innerHTML = banner;
 }
-document.addEventListener("DOMContentLoaded", banner);
+document.addEventListener("DOMContentLoaded", loadBanner);
